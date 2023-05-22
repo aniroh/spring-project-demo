279fac3e862e995391a5fa06d6b6e23dcf9018d8
Anish <rohekar.anish@gmail.com>
2023-05-22 11:16:33 +0530

    custom properties using application.properties

--- myapp1/src/main/java/com/example/myapp1/rest/FunRestController.java
+++ myapp1/src/main/java/com/example/myapp1/rest/FunRestController.java
@@ -1,13 +1,27 @@
 package com.example.myapp1.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    // inject properties for ani.username and ani.profession from application.properties - custom properties
+    @Value("${ani.surname}")
+    private String aniSurname;
+
+    @Value("${ani.profession}")
+    private String aniProf;
+
+    //new endpoint to display custom properties - custom properties
+    @GetMapping("/aniinfo")
+    public String AniInfo(){
+        return "Anish "+ aniSurname+" works as a "+aniProf;
+    }
+
     @GetMapping("/")
     public String SayHello(){
-        return "Hello world";
+        return "Hello World";
     }
 
 
